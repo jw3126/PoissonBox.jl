b71ecef29aa4707e5dad2b833f248c166c4c6719
Jan Weidner <jw3126@gmail.com>
2022-03-03 10:42:14 +0100

    init

--- src/PoissonBox.jl
+++ src/PoissonBox.jl
@@ -1,5 +1,126 @@
 module PoissonBox
 
-# Write your package code here.
+using TiledIteration
+import LinearAlgebra
+import IterativeSolvers
+
+function CartesianIndices_inner(x::AbstractArray)
+    ranges = map(axes(x)) do ax
+        (first(ax)+1):(last(ax)-1)
+    end
+    CartesianIndices(ranges)
+end
+
+function forward!(out,u)
+    if axes(out) != axes(u)
+        msg = """
+        Axes do not match:
+        axes(out) == $(axes(out))
+        axes(u) == $(axes(u))
+        """
+        throw(DimensionMismatch(msg))
+    end
+    all_inds = CartesianIndices(u)
+    inner_inds = CartesianIndices_inner(u)
+    begin
+        for I in inner_inds
+            out[I] = laplace(u, I)
+        end
+        for I in EdgeIterator(all_inds, inner_inds)
+            out[I] = u[I]
+        end
+    end
+    return out
+end
+function forward(u)
+    out = similar(u, float(eltype(u)))
+    forward!(out, u)
+end
+
+Base.@propagate_inbounds function laplace(u::AbstractArray{<:Any,1},I::CartesianIndex)
+    (i,) = Tuple(I)
+    (u[i+1] + u[i-1])/2 - u[i]
+end
+
+Base.@propagate_inbounds function laplace(u::AbstractArray{<:Any,2},I::CartesianIndex)
+    (i,j) = Tuple(I)
+    (u[i+1,j] + u[i-1,j] + u[i,j+1] + u[i, j-1])/4 - u[i,j]
+end
+Base.@propagate_inbounds function laplace(u::AbstractArray{<:Any,3},I::CartesianIndex)
+    (i,j,k) = Tuple(I)
+    (u[i+1,j  ,k  ] + u[i-1,j   ,k  ] + 
+     u[i  ,j+1,k  ] + u[i  , j-1,k  ] +
+     u[i  ,j  ,k+1] + u[i  , j  ,k-1]
+    )/6 - u[i,j,k]
+end
+
+################################################################################
+#### LaplaceDirichlet
+################################################################################
+struct LaplaceDirichlet{T,N}
+    size::NTuple{N,Int}
+end
+
+make_eltype_float(x::AbstractArray{<:AbstractFloat}) = x
+function make_eltype_float(x::AbstractArray)
+    T = float(eltype(x))
+    out = similar(x, T)
+    copy!(out, x)
+end
+
+
+function poisson_problem(arr::AbstractArray{T,N}) where {T,N}
+    op = LaplaceDirichlet{float(T),N}(size(arr))
+    v = reshape(make_eltype_float(arr), length(arr))
+    op, v
+end
+function reshape_solution(op, sol)
+    reshape(sol, op.size)
+end
+
+function LinearAlgebra.mul!(out, op::LaplaceDirichlet, v::AbstractVector)
+    forward!(reshape(out, op.size), reshape(v, op.size))
+    out
+end
+function Base.:(*)(op::LaplaceDirichlet, v::AbstractVector)
+    T = typeof(one(eltype(v))/one(eltype(op)))
+    out = similar(v,T)
+    mul!(out, op, v)
+end
+function Base.eltype(::Type{LaplaceDirichlet{T,N}}) where {T,N}
+    T
+end
+function Base.size(op::LaplaceDirichlet, d)
+    (1,2)[d] # for appropriate error
+    prod(op.size)
+end
+
+function Base.collect(::Type{T}, op::LaplaceDirichlet) where {T}
+    # useful for debugging
+    n = size(op,1)
+    out = Matrix{T}(undef, n,n)
+    v = zeros(Int,n)
+    for i in 1:n
+        v[i] = 1
+        if i > 1
+            v[i-1] = 0
+        end
+        mul!(view(out,:,i), op, v)
+    end
+    out
+end
+function Base.collect(op::LaplaceDirichlet)
+    Base.collect(eltype(op), op)
+end
+
+function solve(solver, y::AbstractArray, solver_args...; solver_kw...)
+    op, v = poisson_problem(y)
+    sol = solver(op, v, solver_args...; solver_kw...)
+    reshape_solution(op, sol)
+end
+function solve(y::AbstractArray)
+    solve(IterativeSolvers.bicgstabl, y, 2)
+end
 
 end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,72 @@
 using PoissonBox
 using Test
+using IterativeSolvers
+import PoissonBox as PB
+using Random: Xoshiro
 
-@testset "PoissonBox.jl" begin
-    # Write your tests here.
+
+@testset "laplace eqn operator" begin
+    forward = PoissonBox.forward
+    @testset "1d" begin
+        @test forward(1:5) ≈ [1,0,0,0,5]
+        @test forward([0,0,1,0,0]) ≈ [0,0.5,-1,0.5,0]
+    end
+    @testset "2d" begin
+        @test forward(
+            [0 0 0 0 0
+                 0 0 0 0 0
+                 0 0 1 0 0
+                 0 0 0 0 0 
+                 0 0 0 0 0 
+                ]) ≈ [
+                 0 0    0    0    0
+                 0 0    0.25 0    0
+                 0 0.25 -1   0.25 0
+                 0 0    0.25 0    0 
+                 0 0    0    0    0 
+                ]
+    end
+    @testset "3d" begin
+        arr = fill(0, (5,5,5))
+        arr[3,3,3] = 1
+        expected = fill(0.0,(5,5,5))
+        expected[3,3,3] = -1
+        expected[2,3,3] = 1/6
+        expected[4,3,3] = 1/6
+        expected[3,2,3] = 1/6
+        expected[3,4,3] = 1/6
+        expected[3,3,2] = 1/6
+        expected[3,3,4] = 1/6
+        @test forward(arr) ≈ expected
+    end
+    @testset "3d bdry" begin
+        arr = randn(3,4,5)
+        out = forward(arr)
+        expected = copy(arr)
+        expected[2:2, 2:3, 2:4] .= 0
+        out[2:2, 2:3, 2:4] .= 0
+        @test expected ≈ out
+    end
+end
+
+@testset "solve" begin
+    solve = PoissonBox.solve
+    y = Float64[1, 0, 0, 0, 5]
+    @test solve(y) ≈ 1:5
+    for shape in [(5,),
+                  (4,3),
+                  (5,4),
+                  (4,3,4),
+                 ]
+        y = rand(Xoshiro(0), shape...)
+        x = solve(y)
+        @test PB.forward(x) ≈ y rtol=1e-3
+    end
+end
+
+@testset "solve low level" begin
+    op, v = PB.poisson_problem([1, 0, 0, 0, 5])
+    sol = bicgstabl(op, v, 2)
+    u = PB.reshape_solution(op, sol)
+    @test u ≈ 1:5
 end
