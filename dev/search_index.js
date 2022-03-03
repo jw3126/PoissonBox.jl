1b2a59299b34bd47a7778d02ed1903cbcc984c70
Documenter.jl <documenter@juliadocs.github.io>
2022-03-03 09:27:45 +0000

    build based on a594798

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PoissonBox","category":"page"},{"location":"#PoissonBox","page":"Home","title":"PoissonBox","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PoissonBox.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PoissonBox]","category":"page"}]
+}
